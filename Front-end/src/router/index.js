be3376838890e0b84311ade40f4b2c53ee4e4420
dongtp-tts <dongtp_tts@rikkeisoft.com>
2022-07-08 11:45:24 +0700

    push module register none handle register

--- Front-end/src/router/index.js
+++ Front-end/src/router/index.js
@@ -1,8 +1,12 @@
 import Home from "../page/home"
+import Register from "../page/register"
 
 const publicRouters = [
     {
         path: '/', component: Home
+    },
+    {
+        path: '/register', component: Register
     }
 ]
 
