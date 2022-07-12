fcf66567dd569791d64a71491d496e1f2b863913
dongtp-tts <dongtp_tts@rikkeisoft.com>
2022-07-12 08:43:19 +0700

    update-register

--- Front-end/src/page/register/index.js
+++ Front-end/src/page/register/index.js
@@ -8,9 +8,12 @@ import InputLabel from "@mui/material/InputLabel";
 import React, { useState } from "react";
 import styles from "./Register.module.scss";
 import FormError from "../../components/formError";
+<<<<<<< HEAD
 import { registerUser } from "../../services";
 import { useNavigate } from "react-router-dom";
 import BackdropLoading from "../../components/backDrop";
+=======
+>>>>>>> fd9fb09374486e9cf1e7fa3ab9cb80d2dd4783c4
 
 const Register = () => {
   const [openLoading, setOpenLoading] = useState(false);
