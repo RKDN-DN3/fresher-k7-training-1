d95a7fb607cbce12caf294da891e9d5bd01e1a0d
quanglbtts <quanglb_tts@rikkeisoft.com>
2022-07-14 14:40:00 +0700

    edit-updateuser

--- Back-end/ToDo.Sample.DataAccess/Services/ProfileUserServices/ProfileUserServices.cs
+++ Back-end/ToDo.Sample.DataAccess/Services/ProfileUserServices/ProfileUserServices.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,22 +15,22 @@ namespace ToDo.Sample.DataAccess.Services.ProfileUserServices
     {
         private readonly UserManager<ApiUser> _userManager;
         private readonly IClaimUserServices _claimUserServices;
+        private readonly ResponseDto _responseDto;
  
-
-        public ProfileUserServices(UserManager<ApiUser> userManager, IClaimUserServices claimUserServices)
+        public ProfileUserServices(UserManager<ApiUser> userManager, 
+            IClaimUserServices claimUserServices)
         {
             _userManager = userManager;
             _claimUserServices = claimUserServices;
-
+            _responseDto = new ResponseDto();
         }
 
         public async Task<ResponseDto> GetUser()
         {
-            var response = new ResponseDto();
             try
             {
                 var currentUserId = _claimUserServices.GetCurrentUserId();
-                response.Result = _userManager.Users
+                _responseDto.Result = _userManager.Users
                     .Where(c => c.Id == currentUserId)
                     .Select(c => new UserForProfileDto()
                     {
@@ -42,35 +43,37 @@ namespace ToDo.Sample.DataAccess.Services.ProfileUserServices
             }
             catch (Exception ex)
             {
-                response.IsSuccess = false;
-                response.ErrorMessages = ex.ToString();
+                _responseDto.IsSuccess = false;
+                _responseDto.ErrorMessages = ex.ToString();
             }
             await Task.CompletedTask;
-            return response;
+            return _responseDto;
         }
 
         public async Task<ResponseDto> UpdateUser(UpdateUserDto user)
         {
-            var response = new ResponseDto();
             try
             {
                 var currentUserId = _claimUserServices.GetCurrentUserId();
                 if (currentUserId == null)
                 {
-                    response.IsSuccess = false;
-                    response.ErrorMessages = "User not exist in system";
+                    _responseDto.IsSuccess = false;
+                    _responseDto.ErrorMessages = "User not exist in system";
+                    return _responseDto;
                 }
-                var userInDn = await _userManager.FindByIdAsync(currentUserId);
-                await _userManager.SetUserNameAsync(userInDn, user.UserName);
-                await _userManager.SetPhoneNumberAsync(userInDn, user.PhoneNumber);
-                response.Result = user;
+                var userInDb = await _userManager.FindByIdAsync(currentUserId);
+                userInDb.FullName = user.FullName;
+                userInDb.PhoneNumber = user.PhoneNumber;
+                await _userManager.UpdateAsync(userInDb);
+
+                _responseDto.Result = user;
             }
             catch (Exception ex)
             {
-                response.IsSuccess = false;
-                response.ErrorMessages = ex.ToString();
+                _responseDto.IsSuccess = false;
+                _responseDto.ErrorMessages = ex.ToString();
             }
-            return response;
+            return _responseDto;
         }
     }
 }
--- Back-end/ToDo.Sample.Models/Dto/UserDto.cs
+++ Back-end/ToDo.Sample.Models/Dto/UserDto.cs
@@ -40,7 +40,7 @@ namespace ToDo.Sample.Models.Dto
     }
     public class UpdateUserDto
     {
-        public string UserName { get; set; }
+        public string FullName { get; set; }
         public string PhoneNumber { get; set; }
     }
 }
