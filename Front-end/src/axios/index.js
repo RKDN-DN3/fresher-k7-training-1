9840178319d6fcaa62536e4b24829d8cb361b1d9
tientrantv <tientv_tts@rikkeisoft.com>
2022-07-19 15:21:26 +0700

    fix config

--- Front-end/src/axios/index.js
+++ Front-end/src/axios/index.js
@@ -9,19 +9,4 @@ axiosClient.defaults.headers = {
     Accept: 'application/json',
 };
 
-axiosClient.interceptors.request.use(async (config) => config);
-
-axiosClient.interceptors.response.use(
-    (response) => {
-        if (response && response.data) {
-            return response.data;
-        }
-
-        return response;
-    },
-    (error) => {
-        throw error;
-    },
-);
-
 export default axiosClient;
